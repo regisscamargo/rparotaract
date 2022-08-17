6c16ce0823d68da5815ce49a79ffc6dde138e6a0
Régis Camargo <regisscamargo@hotmail.com>
2022-08-17 16:10:13 -0300

    Atualização de código robo 01 #01

--- Robo01.py
+++ Robo01.py
@@ -13,51 +13,83 @@ from collections import namedtuple
 import json
 import time as t
 
-url = 'https://bnccompras.com/Process/ProcessSearchPublic?param1=1'
 
+# Case são exatamente isso, cases, informações em blocos para serem selecionadas antes do codigo
 
-options = ChromeOptions()
-path_chromedriver = "C:\Projeto\chromedriver.exe"
-prefs = {"download.default_directory": "C:\\Macros\\download"}
-options.add_experimental_option("prefs", prefs)
+login = "login1"
 
+match login: 
+    case 'login1':
+        login = "abcbolinbhas123"
+        senha = "123"
+
+    case 'login2':
+        login = "abcbolinbhas123"
+        senha = "123"
+    
+    case 'login3':
+        login = "abcbolinbhas123"
+        senha = "123"
+
+    case 'login4':
+        login = "abcbolinbhas123"
+        senha = "123"
 
 ####### Inicio do código ######
 
+link = 'https://www.instagram.com/'
+
+options = ChromeOptions()
+path_chromedriver = "C:\Chrome Driver\chromedriver.exe"
+prefs = {"download.default_directory": "C:\\Macros\\download"}
+options.add_experimental_option("prefs", prefs)
+navegador = Chrome(chrome_options=options)
+
 # Essa parte é responsavel por abrir o navegador e ir até a pasta desejada
 
-def inicialização (data) -> None:
-    
-    # Iniciando o Chorme (navegador de preferencia, mas RPA chrome é o recomendado)
-    navegador = Chrome(
-            options=options, executable_path=f"{path_chromedriver}")
-    
-    # Setando o link que o navegador deve abrir 
-    navegador.get = 'https://www.instagram.com/'
-
-    # Informaçõe usadas durante o código, como login, senha, usuário de procura
-    user = ''
-    senha = ''
-
-    usuario_pesquisar = 'eita_regis'
-
-    #Seletor do campo desejado para informar o que queremos fazer
-    #Seletor #Path - //*[@id="loginForm"]/div/div[1]/div/label/input
-    campo_login = navegador.find_element(By. XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
-    
-    #Seletor #CSS Selector - #loginForm > div > div:nth-child(1) > div > label > input
-    campo_login = navegador.find_element(By. CSS_SELECTOR, '#loginForm > div > div:nth-child(1) > div > label > input')
-    
-    #Seletor #ClassName 
-    campo_login = navegador.find_element(By. CLASS_NAME, '#login')
-
-    #Seletor #Id
-    campo_login = navegador.find_element(By. ID, 'login')
+def inicializaçao():
 
 
-    campo_login.send_keys(user)
+        # Iniciando o Chorme (navegador de preferencia, mas RPA chrome é o recomendado)
+        # Setando o link que o navegador deve abrir 
+        navegador.get = link
+        navegador.maximize_window()
 
-    
+        # Informaçõe usadas durante o código, como login, senha, usuário de procura
+        user = ''
+        senha = ''
 
+        usuario_pesquisar = 'eita_regis'
 
+        try:
+
+                #Seletor do campo desejado para informar o que queremos fazer
+                #Seletor #Path - //*[@id="loginForm"]/div/div[1]/div/label/input
+                campo_login = navegador.find_element(By. XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
+                campo_login.send_keys(user)
+                
+                #Seletor #CSS Selector - #loginForm > div > div:nth-child(1) > div > label > input
+                campo_login2 = navegador.find_element(By. CSS_SELECTOR, '#loginForm > div > div:nth-child(1) > div > label > input')
+                
+                #Seletor #ClassName 
+                campo_login3 = navegador.find_element(By. CLASS_NAME, '#login')
+
+                #Seletor #Id
+                campo_login4 = navegador.find_element(By. ID, 'login')
+
+                #Campo de preenchimento de senha
+                campo_senha = navegador.find_element(By. XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
+                campo_senha.send_keys(senha)
+
+                botao_entrar = navegador.find_element(By. XPATH, '#loginForm > div > div:nth-child(3) > button')
+
+                if botao_entrar != None:
+                        botao_entrar.click()
+                
+                else:
+                        print('O login está incorreto')
+
+        except Exception as e:
+                print(e)
+                #Esse print ele informa qual o nome do erro que esta condito dentro TryExpept
 
