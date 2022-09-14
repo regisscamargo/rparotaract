a6234b7ec28559766865b581d5ae8b7ea0a15fc3
Régis Camargo <regisscamargo@hotmail.com>
2022-09-14 13:56:53 -0300

    wip

--- Robo01.py
+++ Robo01.py
@@ -20,21 +20,29 @@ login = "login1"
 
 match login: 
     case 'login1':
-        login = "abcbolinbhas123"
+        nome = "Jãozinho"
+        sobre_nome = "da Silva"
+        instituiçao = "Objetivo"
+        email = "jaozinhodasilva@abcbolinhas.com"
+        user = "jaozinho"
         senha = "123"
 
     case 'login2':
-        login = "abcbolinbhas123"
+        nome = "Jãozinho"
+        sobre_nome = "da Silva"
+        instituiçao = "Objetivo"
+        email = "jaozinhodasilva@abcbolinhas.com"
+        user = "jaozinho"
         senha = "123"
+
     
     case 'login3':
-        login = "abcbolinbhas123"
+        nome = "Jãozinho"
+        sobre_nome = "da Silva"
+        instituiçao = "Objetivo"
+        email = "jaozinhodasilva@abcbolinhas.com"
+        user = "jaozinho"
         senha = "123"
-
-    case 'login4':
-        login = "abcbolinbhas123"
-        senha = "123"
-
 ####### Inicio do código ######
 
 
@@ -54,44 +62,56 @@ def inicializaçao():
         options.add_argument('profile-directory=Profile')
         driver = webdriver.Chrome(chrome_options=options, service=Service(ChromeDriverManager().install()))
 
-        driver.get('https://www.instagram.com/')
+        driver.get('https://www.gridlastic.com/register.php')
         t.sleep(10)
         driver.maximize_window()
 
         # Informaçõe usadas durante o código, como login, senha, usuário de procura
-        user = 'regis'
-        senha = ''
-
-        usuario_pesquisar = 'eita_regis'
 
         try:
+            campo_nome = driver.find_element(By. XPATH, '/html/body/div[2]/form/div[1]/div/div/input')
+            campo_nome.send_keys(nome)
 
-                #Seletor do campo desejado para informar o que queremos fazer
-                #Seletor #Path - //*[@id="loginForm"]/div/div[1]/div/label/input
-                campo_login = driver.find_element(By. XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
-                campo_login.send_keys(user)
+            print("Nome preenchido")
+            t.sleep(3)
+
+            campo_sobrenome = driver.find_element(By. XPATH, '/html/body/div[2]/form/div[2]/div/div/input')
+            campo_sobrenome.send_keys(sobre_nome)
+
+            print("Sobrenome preenchido")
+            t.sleep(3)
+
+            campo_instituicao = driver.find_element(By. XPATH, '/html/body/div[2]/form/div[3]/div/div/input')
+            campo_instituicao.send_keys(instituiçao)
+
+            print("Instituição preenchida")
+            t.sleep(3)
+
+            campo_email = driver.find_element(By. XPATH, '/html/body/div[2]/form/div[4]/div/div/input')
+            campo_email.send_keys(email)
+
+            print("E-mail preenchido")
+            t.sleep(3)
+
+            campo_username = driver.find_element(By. XPATH, '/html/body/div[2]/form/div[5]/div/div/input')
+            campo_username.send_keys(user)
+
+            print("User preenchido")
+            t.sleep(3)
+
+            campo_senha = driver.find_element(By. XPATH, '//*[@id="register_password"]')
+            campo_senha.send_keys(senha)
+
+            print("Senha preenchida")
+            t.sleep(3)
+
+            print("Cadastro Finalizado com sucesso")
+
+            t.sleep(300)
+
+            bota_criarconta =  driver.find_element(By. XPATH, '//*[@id="register-submit-btn"]')
+            bota_criarconta.click()
                 
-                #Seletor #CSS Selector - #loginForm > div > div:nth-child(1) > div > label > input
-                campo_login2 = driver.find_element(By. CSS_SELECTOR, '#loginForm > div > div:nth-child(1) > div > label > input')
-                
-                #Seletor #ClassName 
-                campo_login3 = driver.find_element(By. CLASS_NAME, '#login')
-
-                #Seletor #Id
-                campo_login4 = driver.find_element(By. ID, 'login')
-
-                #Campo de preenchimento de senha
-                campo_senha = driver.find_element(By. XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
-                campo_senha.send_keys(senha)
-
-                botao_entrar = driver.find_element(By. XPATH, '#loginForm > div > div:nth-child(3) > button')
-
-                if botao_entrar != None:
-                        botao_entrar.click()
-                
-                else:
-                        print('O login está incorreto')
-
         except Exception as e:
                 print(e)
                 #Esse print ele informa qual o nome do erro que esta condito dentro TryExpept
